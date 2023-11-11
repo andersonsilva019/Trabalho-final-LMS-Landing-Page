f47ca333c33101d908b4ce1b3b65070a27053013
Anderson Silva <andersonnsilva@alu.ufc.br>
2023-11-11 15:27:57 -0300

    Removendo console.log

--- js/feedback_scroll.js
+++ js/feedback_scroll.js
@@ -3,37 +3,37 @@ var currPage = 0;
 
 var comments = [
     {
-        "username" : "Chico",
-        "photo" : null,
-        "role" : "Founder",
-        "company" : "arceus co. ltd",
-        "comment" : "LOREM IPSUM"
+        "username": "Chico",
+        "photo": null,
+        "role": "Founder",
+        "company": "arceus co. ltd",
+        "comment": "LOREM IPSUM"
     },
     {
-        "username" : "Paulo",
-        "photo" : null,
-        "role" : "Founder",
-        "company" : "arceus co. ltd",
-        "comment" : "LOREM IPSUM"
+        "username": "Paulo",
+        "photo": null,
+        "role": "Founder",
+        "company": "arceus co. ltd",
+        "comment": "LOREM IPSUM"
     },
     {
-        "username" : "Durgivaldo",
-        "photo" : null,
-        "role" : "Founder",
-        "company" : "arceus co. ltd",
-        "comment" : "LOREM IPSUM"
+        "username": "Durgivaldo",
+        "photo": null,
+        "role": "Founder",
+        "company": "arceus co. ltd",
+        "comment": "LOREM IPSUM"
     },
     {
-        "username" : "Yslatovek",
-        "photo" : null,
-        "role" : "Founder",
-        "company" : "arceus co. ltd",
-        "comment" : "LOREM IPSUM"
+        "username": "Yslatovek",
+        "photo": null,
+        "role": "Founder",
+        "company": "arceus co. ltd",
+        "comment": "LOREM IPSUM"
     }
 ]
 
 
-function scrollComentsForward(){
+function scrollComentsForward() {
     currPage = (currPage + 1) % comments.length;
 
     let itensToShow = [
@@ -46,7 +46,7 @@ function scrollComentsForward(){
 
 }
 
-function scrollComentsBack(){
+function scrollComentsBack() {
     currPage = (currPage - 1 + comments.length) % comments.length;
 
 
@@ -59,23 +59,22 @@ function scrollComentsBack(){
     displayAtContainer(itensToShow);
 }
 
-function displayAtContainer(itens){
+function displayAtContainer(itens) {
     const defaultProfileImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrT_BjEyf_LEpcvb225JX2qFCJcLz5-0RXLg&usqp=CAU"
-    console.log("DISPLAYING");
     const commentsContainer = document.getElementById("feedback_container");
     let firstCommentFlag = true;
     let htmlContent = "";
 
     itens.forEach(element => {
-        htmlContent += 
-        "<div class=\"comment_card\"" + (firstCommentFlag ? " id=\"first_comment\"" : "") + ">\n" +
-        "   <img src=" + (element.photo == null ? defaultProfileImage : element.photo) + "> </img>\n"+
-        "   <p>\"" + element.comment + "\"</p>\n"+
-        "    <div>\n"+
-        "        <p style=\"font-weight: bolder;\">" + element.username + "</p>\n" +
-        "        <p>" + element.role + ", " +element.company + "</p>\n" +
-        "    </div>\n" +
-        "</div>\n\n"
+        htmlContent +=
+            "<div class=\"comment_card\"" + (firstCommentFlag ? " id=\"first_comment\"" : "") + ">\n" +
+            "   <img src=" + (element.photo == null ? defaultProfileImage : element.photo) + "> </img>\n" +
+            "   <p>\"" + element.comment + "\"</p>\n" +
+            "    <div>\n" +
+            "        <p style=\"font-weight: bolder;\">" + element.username + "</p>\n" +
+            "        <p>" + element.role + ", " + element.company + "</p>\n" +
+            "    </div>\n" +
+            "</div>\n\n"
         firstCommentFlag = false;
     });
 
